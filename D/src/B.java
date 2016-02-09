3827c4ef305e4eccf84d124155d4b8551520bb7c
chinmayaasc <chinmaya.satapathy@ascendum.com>
2016-02-09 17:02:01 +0530

    Comit D

---
+++ D/src/B.java
@@ -0,0 +1,23 @@
+
+public class B {
+
+	/*public static void main(String[] args){
+		System.out.println("String array");
+	}*/
+	
+	public static int main(int[] args){
+		System.out.println("String array");
+		return 1;
+	}
+	
+	/*public static String main(String a){
+		System.out.println("String return");
+		return "";
+	}
+	
+	public static void main(int a){
+		System.out.println("int arg");
+	}*/
+	
+	
+}
